4fbe149f64309a52beb3ac8d257504b88b74fe49
christianrrl <christianrod@mail.usf.edu>
2015-11-23 01:05:17 -0500

    Client and Server edits

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,10 +1,50 @@
+import java.io.*; 
+import java.net.*; 
+import java.util.*;
 
-public class UDPClient
-{
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
-	public UDPClient()
-	{
-		// TODO Auto-generated constructor stub
-	}
+class UDPClient {    
+	private static int port = 9001;
+	
+	public static void main(String args[]) throws Exception    
+	{       
+		BufferedReader inFromUser = 
+				new BufferedReader(new InputStreamReader(System.in));
+		DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress IPAddress = InetAddress.getByName("localhost");
+//		InetAddress IPAddress = InetAddress.getByName("10.224.2.155");
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+		
+		while (true)
+		{
+			String sentence = inFromUser.readLine();
+			sendData = sentence.getBytes();
+			DatagramPacket sendPacket = 
+					new DatagramPacket(sendData, sendData.length, IPAddress, port);
+			clientSocket.send(sendPacket);       
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
+			String modifiedSentence = new String(receivePacket.getData());
+			System.out.println("FROM SERVER:" + modifiedSentence);
+		}
+//		Enumeration en = NetworkInterface.getNetworkInterfaces();
+//		while(en.hasMoreElements()){
+//		    NetworkInterface ni=(NetworkInterface) en.nextElement();
+//		    Enumeration ee = ni.getInetAddresses();
+//		    while(ee.hasMoreElements()) {
+//		        InetAddress ia= (InetAddress) ee.nextElement();
+//		        System.out.println(ia.getHostAddress());
+//		        }
+//		    }		
+		}
+	
+	} 
 
-}
+// - See more at: https://systembash.com/a-simple-java-udp-server-and-udp-client/#sthash.ui72SbBg.dpuf
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,10 +1,107 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-public class UDPServer
+class UDPServer
 {
-
-	public UDPServer()
+	private static int port = 9001;
+	public static void main(String args[]) throws Exception
 	{
-		// TODO Auto-generated constructor stub
-	}
+         DatagramSocket serverSocket = new DatagramSocket(port);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+            boolean state = true;
+            boolean temp = false;
+            Vector<String> users = new Vector();
+    		Vector<String> userAdded = new Vector();
 
-}
+            
+//            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+//            while (interfaces.hasMoreElements()){
+//                NetworkInterface current = interfaces.nextElement();
+//                System.out.println(current);
+//                if (!current.isUp() || current.isLoopback() || current.isVirtual()) continue;
+//                Enumeration<InetAddress> addresses = current.getInetAddresses();
+//                while (addresses.hasMoreElements()){
+//                    InetAddress current_addr = addresses.nextElement();
+//                    if (current_addr.isLoopbackAddress()) continue;
+//                    if (current_addr instanceof Inet4Address)
+//                    	  System.out.println(current_addr.getHostAddress());
+////                    	else if (current_addr instanceof Inet6Address)
+////                    	  System.out.println(current_addr.getHostAddress());
+//                }
+//            }
+
+            while(state)
+            {
+            	receiveData = new byte[1024];
+            	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            	serverSocket.receive(receivePacket);
+            	String sentence = 
+            			new String( receivePacket.getData(), 
+            					receivePacket.getOffset(), 
+            					receivePacket.getLength(), "UTF-8");
+            	System.out.println("RECEIVED: " + sentence);
+            	InetAddress IPAddress = receivePacket.getAddress();
+            	int port = receivePacket.getPort();
+            	String capitalizedSentence = sentence.toUpperCase();
+//            	sendData = capitalizedSentence.getBytes();
+//            	DatagramPacket sendPacket =
+//            			new DatagramPacket(sendData, sendData.length, IPAddress, port);
+//            	serverSocket.send(sendPacket);
+            	
+            	if (capitalizedSentence.equals("OFF"))
+            	{
+                	  state = false;
+                	  System.out.println("Server shutting down");
+                	  serverSocket.close();
+            	}
+            	
+            	String[] commands = sentence.split(";");
+            	for (int i = 0; i < commands.length; i++)
+            	{
+            		System.out.print("Command " + i + ": ");
+            		
+            		String[] parts = commands[i].split(",");
+            		
+        			if (parts[0].equals("REG"))
+        			{
+        				if (!users.contains(parts[1].toUpperCase()))
+        				{
+        					users.add(parts[1].toUpperCase());
+//        					userAdded = 1;
+        					System.out.println(users);
+        					
+        	            	sendData = "ROK".getBytes();
+        	            	DatagramPacket sendPacket =
+        	            			new DatagramPacket(sendData, sendData.length, IPAddress, port);
+        	            	serverSocket.send(sendPacket);
+
+        				}
+                		else
+        				{
+//                            byte[] sendData1 = new byte[1024];
+        					System.out.println("Print else");
+//        					String temp1 = new String("UAE");
+//        					sendData = temp1.getBytes();
+//        	            	DatagramPacket sendPacket1 =
+//        	            			new DatagramPacket(sendData, sendData.length, IPAddress, port);
+//        	            	serverSocket.send(sendPacket1);
+        	            	sendData = "UAE".getBytes();
+        	            	DatagramPacket sendPacket =
+        	            			new DatagramPacket(sendData, sendData.length, IPAddress, port);
+        	            	serverSocket.send(sendPacket);
+
+        	            	
+        				}
+        			}
+
+            		
+            		System.out.println();
+//            		System.out.println(commands[i]);
+            	}
+//                  System.out.println("sentence: " + sentence + "capitalizedSentence: " + capitalizedSentence + "asdf");
+               }
+            }   
+	}
+// - See more at: https://systembash.com/a-simple-java-udp-server-and-udp-client/#sthash.ui72SbBg.dpuf
